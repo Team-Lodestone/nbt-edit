a5e05ff631327a24ddc744d1b3a9394e93e06972
Tristan F <LeoDog896@hotmail.com>
2023-03-10 18:20:18 -0500

    feat: array

--- src/lib/Value.svelte
+++ src/lib/Value.svelte
@@ -10,11 +10,28 @@
 		);
 	}
 
+    function isArray(value: unknown): value is unknown[] {
+        return Array.isArray(value) || value instanceof Uint8Array || value instanceof Int8Array
+    }
+
 	export let value: unknown;
+    $: console.log(value)
 </script>
 
 {#if isNumber(value)}
 	{value}
+{:else if isArray(value)}
+    {#if value.length === 0}
+        <span><i>Empty List</i></span>
+    {:else}
+        <ul>
+            {#each value as val, i}
+                <li>
+                    [{i}]: <svelte:self value={val} />
+                </li>
+            {/each}
+        </ul>
+    {/if}
 {:else if typeof value === 'object' && value !== null}
 	<ul>
 		{#each Object.entries(value) as [key, val]}
