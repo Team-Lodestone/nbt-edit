05ba999784cff5b3b63c07568927c01a849bba46
Tristan F <LeoDog896@hotmail.com>
2023-03-10 19:59:47 -0500

    feat: editor

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { EditorView, basicSetup } from "codemirror"
+	import { onMount } from "svelte";
+
+    let container: HTMLDivElement
+
+    onMount(() => {
+        new EditorView({
+            extensions: [basicSetup],
+            parent: container
+        })
+    })
+
+</script>
+
+<div bind:this={container}/>
